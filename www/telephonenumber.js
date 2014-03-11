57fb39be92221cc2a52967b63971a9534a7aa836
Clément Vollet <clement@joshfire.com>
2014-03-11 16:43:40 +0100

    Refactor js file and use js-module

--- www/telephonenumber.js
+++ www/telephonenumber.js
@@ -1,27 +1,11 @@
-cordova.define("cordova/plugin/telephonenumber",
-  function(require, exports, module) {
-    var exec = require("cordova/exec");
-    var TelephoneNumber = function () {};
+var exec = require("cordova/exec");
 
-    var TelephoneNumberError = function(code, message) {
-        this.code = code || null;
-        this.message = message || '';
-    };
+var TelephoneNumber = function () {};
 
-    TelephoneNumber.NO_TELEPHONE_NUMBER = 0;
+TelephoneNumber.NO_TELEPHONE_NUMBER = 0;
 
-    TelephoneNumber.prototype.get = function(success,fail) {
-        exec(success,fail,"TelephoneNumber",
-            "get",[]);
-    };
+TelephoneNumber.prototype.get = function (success, fail) {
+    exec(success,fail,'TelephoneNumber','get',[]);
+};
 
-    var telephoneNumber = new TelephoneNumber();
-    module.exports = telephoneNumber;
-});
-
-if(!window.plugins) {
-    window.plugins = {};
-}
-if (!window.plugins.telephoneNumber) {
-    window.plugins.telephoneNumber = cordova.require("cordova/plugin/telephonenumber");
-}
+module.exports = TelephoneNumber;
