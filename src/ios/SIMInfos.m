7c5f04b5af345b476f50b5a38175e12d37a98d62
Clément Vollet <clement@joshfire.com>
2014-03-12 16:55:57 +0100

    Reindent

--- src/ios/SIMInfos.m
+++ src/ios/SIMInfos.m
@@ -6,25 +6,27 @@
 // Does nothing, impossible on iOS
 - (void)getPhoneNumber:(CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)getCountry:(CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* pluginResult = nil;
-  CTTelephonyNetworkInfo *netInfo = [[CTTelephonyNetworkInfo alloc] init];
-  if (netInfo) {
-    CTCarrier *carrier = [netInfo subscriberCellularProvider];
-    if (carrier) {
-      NSString *result = [carrier mobileCountryCode];
-      if (result) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
-      }
+    CDVPluginResult* pluginResult = nil;
+    CTTelephonyNetworkInfo *netInfo = [[CTTelephonyNetworkInfo alloc] init];
+    if (netInfo) {
+        CTCarrier *carrier = [netInfo subscriberCellularProvider];
+        if (carrier) {
+            NSString *result = [carrier mobileCountryCode];
+            if (result) {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
+            }
+        }
     }
-  }
-  if (!pluginResult) {
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"info not available"];
-  }
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    if (!pluginResult) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"info not available"];
+    }
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+@end
